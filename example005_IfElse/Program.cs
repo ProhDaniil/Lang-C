81d26594fef626dd397c92b85683328d1aea04a8
DaniilProh <daniil06052008@gmail.com>
2023-08-14 18:57:59 +0800

    add Test If-Else

---
+++ example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "полина");
+{
+    Console.WriteLine("Ура, это Полина!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);   
+}
+ 
