6ddedd6bebca27e4c774a3f6fbad9d1eaea4b552
DaniilProh <daniil06052008@gmail.com>
2023-08-14 13:14:17 +0800

    add example N2 HelloUser

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Здравствуйте! Введите ваше имя:");
+string username = Console.ReadLine();
+Console.Write(username);
+Console.WriteLine(", Вы замечательно выглядите! Хорошего дня и настроения!");
