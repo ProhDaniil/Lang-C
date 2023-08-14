2c44f092f368a61847226ddf7804c1ad6f1db4b5
DaniilProh <daniil06052008@gmail.com>
2023-08-14 18:05:46 +0800

    add commintaries for DIV

--- example004_DIV/Program.cs
+++ example004_DIV/Program.cs
@@ -1,3 +1,8 @@
-﻿double NumA = 25;
-double NumB = 5;
+﻿double NumA = new Random().Next(1, 13);
+Console.Write("Число 1: ");
+Console.WriteLine(NumA);
+double NumB = new Random().Next(1, 13);
+Console.Write("Число 2: ");
+Console.WriteLine(NumB);
+Console.Write("Частное чисил равно ");
 Console.WriteLine(NumA / NumB);
