375160ecd5b1d2a7552f2bdee86dae83b1883182
DaniilProh <daniil06052008@gmail.com>
2023-08-14 17:30:41 +0800

    add SUM

---
+++ example003_SumNumbers/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число");
+string NumA = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string NumB = Console.ReadLine();
+string result = NumA + NumB;
+Console.Write("Сумма ваших числел равна ");
+Console.WriteLine(result);
